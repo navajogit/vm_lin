00f0140d910fac02dfd256b9eb49f0c7ad4031cf
navajogit <146178899+navajogit@users.noreply.github.com>
2023-09-27 11:55:49 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -15,8 +15,8 @@ sudo dpkg -i ~/Downloads/portmaster.deb
 sudo apt-get install spice-vdagent -y
 
 # Install Brave Browser
-sudo apt-get install curl
+sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt-get update
-sudo apt-get install brave-browser -y
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt install brave-browser -y
