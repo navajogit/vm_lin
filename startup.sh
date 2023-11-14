376422f6644df7bbafbc8807aa61e01f7e3ba805
navajogit <146178899+navajogit@users.noreply.github.com>
2023-11-14 16:30:31 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,218 +1,34 @@
 #!/bin/bash
 
-doner () {
-echo -e "\033[0;32mDONE \033[0m"
-}
-updatesys () {
-    sudo apt update -y
-    sudo apt upgrade -y
-    sudo apt autoremove -y
+# Function to decrypt text
+decrypt_text() {
+    echo -n "$1" | base64 -d | openssl enc -d -aes-256-cbc -a -pbkdf2 -k "$password" | sed "s/@@@@@@@/\n/g"
 }
 
-
-
-ipvchange () {
-
-    ipv6_setting=$(gsettings get org.gnome.system.network ipv6-method)
-
-    if [ "$ipv6_setting" != "'disabled'" ]; then
-        echo "Disabling IPv6 ..."    
-        gsettings set org.gnome.system.network ipv6-method 'disabled'    
-        echo " IPv6 'disabled'."
-    else
-        echo "IPv6 is set to 'disabled'."
-    fi
-
-    nmcli connection modify "Wired connection 1" ipv6.method disabled
-    sudo systemctl restart NetworkManager
-    sleep 1
-
+# Function to securely clear sensitive data
+clear_sensitive_data() {
+    unset password
+    unset decrypted_url
+    unset script_url
 }
 
-basicdeps () {
-    sudo apt install curl -y
-    sudo apt install wget -y
+# Decrypting the URL of the script to download
+echo "Enter password:"
+read -s password
+decrypted_url=$(decrypt_text "VTJGc2RHVmtYMTlpT2o5a3dMd3Y2UDVZbjVMN2F3cUV1Q3pWRFJ4dlNvKzNRcVhFc2ZWbWV0M0ovcFRkZ1dHSworOEQ2TTd4RFVQVy9jTkFmU2IzTmlZVkU4WnRqNmh5a1lGWWd4NTIyUGVvPQo=")
+script_url=$(echo -n "$decrypted_url" | sed "s/@@@@@@@/\n/g")
 
-}
+# Downloading the script from the decrypted URL
+encrypted_script=$(curl -sL "$script_url")
 
-firewallinst () {
-    sudo apt install ufw -y
-    sudo ufw enable
-    sudo ufw limit 22/tcp
-    sudo ufw allow 80/tcp
-    sudo ufw allow 443/tcp
-    sudo ufw default deny incoming
-    sudo ufw default allow outgoing
-    sudo apt-get install git -y
+# Decrypting the downloaded script
+decrypted_script=$(decrypt_text "$encrypted_script")
 
-}
-
-
-vpninst () {
-    wget "https://windscribe.com/install/desktop/linux_deb_x64/windscribe_2.7.14_amd64.deb" -O ~/Downloads/windscribe.deb
-    sudo dpkg -i ~/Downloads/windscribe.deb &&
-    shred -uz ~/Downloads/windscribe.deb
-
-}
-
-firewalliiinst () {
-    sudo apt install libnetfilter-queue1 -y
-    wget "https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb" -O ~/Downloads/portmaster.deb
-    sudo dpkg -i ~/Downloads/portmaster.deb
-    sudo systemctl enable portmaster
-    shred -uzv ~/Downloads/portmaster.deb
-
-}
-
-braveinst () {
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt update -y
-    sudo apt install brave-browser -y
-
-}
-
-guestinst () {
-    sudo apt install liburing2 -y
-    sudo apt install qemu-guest-agent -y
-    sudo apt-get install spice-vdagent -y
-
-}
-
-gitinst () {
-    sudo apt install git -y
-
-}
-
-aescryptinst () {
-if ! type "aescrypt" &>/dev/null; then
-    clear
-    echo "Aescrypt is not installed. Installing..."
-    cd ~/Downloads
-    wget https://github.com/navajogit/aescrypt/raw/main/aescrypt-3.16.tgz &&
-    tar -xzf aescrypt-3.16.tgz &&
-    cd aescrypt-3.16
-    make &&
-    sudo make install &&
-    shred -uz ~/Downloads/aescrypt-3.16.tgz
-    rm -rf ~/Downloads/aescrypt/
-    cd ~
-    echo "Aescrypt was installed."
+# Running the decrypted script in the current session
+if [[ -n "$decrypted_script" ]]; then
+    bash -c "$decrypted_script"
+    clear_sensitive_data
 else
-    clear
-    echo "Aescrypt is already installed on your system."
+    echo "Unable to decrypt and run the script. Check the password or URL."
+    clear_sensitive_data
 fi
-}
-
-appearance () {
-# wallpapers
-githubRepoUrl="https://raw.githubusercontent.com/navajogit/vm_lin/main/sample_list.txt"
-wallpaperUrls=$(curl -s "$githubRepoUrl")
-
-while true
-do
-    echo -e "\033[0;32mDo you want to change the desktop wallpaper? (Y/N)\033[0m"
-    read changeWallpaper
-    if [[ "$changeWallpaper" =~ ^[Yy]$ ]]; then
-        # Choose a random URL
-        randomUrl=$(shuf -n 1 <<< "$wallpaperUrls")
-        wallpaperPath="/home/$USER/Pictures/wallpaper.jpg"
-        wget -progress=bar:force -O "$wallpaperPath" "$randomUrl"
-        gsettings set org.gnome.desktop.background picture-uri-dark "file://$wallpaperPath"
-        clear
-        echo "DONE"
-    else
-        clear
-        echo "DONE"
-        break
-    fi
-done
-}
-
-torinst () {
-wget https://www.torproject.org/dist/torbrowser/13.0/tor-browser-linux-x86_64-13.0.tar.xz -O tor-browser-linux-x86_64-13.0.tar.xz
-tar -xf tor-browser-linux-x86_64-13.0.tar.xz
-chmod +x tor-browser-linux-x86_64-13.0.tar.xz
-cd ~/tor-browser
-./start-tor-browser.desktop --register-app
-echo "alias tor='cd ~/tor-browser && ./start-tor-browser.desktop'" >> ~/.bashrc
-echo "alias tor='cd ~/tor-browser && ./start-tor-browser.desktop'" >> ~/.zshrc
-}
-
-installhist () {
-    echo ""
-    echo "install history:"
-    cat /var/log/dpkg.log.1 /var/log/dpkg.log | grep " install " | nl
-
-}
-
-
-menu () {
-    while true
-    do
-        clear
-        cd ~
-        echo -e "\033[0;32mCHOOSE OPTIONS TO SETUPS:\033[0m"
-        echo ""
-        echo ""
-        echo -e "\033[0;32m0.	UPDATE SYSTEM \033[0m"
-        echo ""
-        echo -e "\033[0;32m1.	IPV6 TO IPV4 \033[0m"
-        echo -e "\033[0;32m2.	FIREWALL \033[0m"
-        echo -e "\033[0;32m3.	FIREWALL II (PORTMASTER) \033[0m"
-        echo -e "\033[0;32m4.	BASIC DEPS \033[0m"
-        echo -e "\033[0;32m5.	VPN \033[0m"
-        echo -e "\033[0;32m6.	BRAVE-BROWSER \033[0m"
-        echo -e "\033[0;32m7.	GUEST ADDITIONS FOR VM \033[0m"
-        echo -e "\033[0;32m8.	GIT \033[0m"
-        echo -e "\033[0;32m9.	AESCRYPT \033[0m"
-        echo -e "\033[0;32m10.	APPEARANCE \033[0m"
-        echo ""
-        echo -e "\033[0;32m11.	TOR BROWSER \033[0m"
-        echo ""
-        echo -e "\033[0;32m12.	OPTIONS 1-11 \033[0m"
-        echo -e "\033[0;32m13.	ALL 0-11 \033[0m"
-        echo ""
-        echo -e "\033[0;32m14.	SHOW INSTALL HISTORY \033[0m"
-        echo ":"
-        read option
-       
-        if [ ${option} -eq 0 ]; then
-            clear && updatesys
-        elif [ ${option} -eq 1 ]; then
-            ipvchange
-        elif [ ${option} -eq 2 ]; then
-            firewallinst
-        elif [ ${option} -eq 3 ]; then
-            firewalliiinst
-        elif [ ${option} -eq 4 ]; then 
-            basicdeps
-        elif [ ${option} -eq 5 ]; then
-            vpninst
-        elif [ ${option} -eq 6 ]; then 
-            braveinst
-        elif [ ${option} -eq 7 ]; then 
-            guestinst
-        elif [ ${option} -eq 8 ]; then 
-            gitinst
-        elif [ ${option} -eq 9 ]; then 
-            aescryptinst
-        elif [ ${option} -eq 10 ]; then 
-            appearance
-        elif [ ${option} -eq 11 ]; then 
-            torinst
-        elif [ ${option} -eq 12 ]; then 
-            ipvchange && firewallinst && firewalliiinst && basicdeps && vpninst && braveinst && guestinst && gitinst && aescryptinst && appearance && torinst
-        elif [ ${option} -eq 13 ]; then 
-            updatesys && ipvchange && firewallinst && firewalliiinst && basicdeps && vpninst && braveinst && guestinst && gitinst && aescryptinst && appearance && torinst 
-        elif [ ${option} -eq 14 ]; then
-            installhist
-        else
-            echo "wrong option"
-        fi
-        echo "Press any key to continue..."
-        read -n 1 -s
-    done
-}
-
-menu
